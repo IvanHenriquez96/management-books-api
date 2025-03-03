9e30434a27405934e51217162083377ce9e51896
Ivan Henriquez <iv.henriquez.96@gmail.com>
2025-03-03 19:11:14 -0300

    connecting to Mongo Atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,21 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { BookModule } from './modules/book/book.module';
 import { ReviewModule } from './modules/review/review.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [AuthModule, UserModule, BookModule, ReviewModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    AuthModule, 
+    UserModule, 
+    BookModule, 
+    ReviewModule,
+    MongooseModule.forRoot(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.${process.env.DB_HOST}`)
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
