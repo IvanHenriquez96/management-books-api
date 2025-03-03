fe9d44350441c8d50ac7d3fd8e514e4ffd783002
Ivan Henriquez <iv.henriquez.96@gmail.com>
2025-03-03 18:43:02 -0300

    modules created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { BookModule } from './modules/book/book.module';
+import { ReviewModule } from './review/review.module';
+
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BookModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
