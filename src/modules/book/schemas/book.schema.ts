5867bc87c4eeff392944eb53e98c59e5a135b302
Ivan Henriquez <iv.henriquez.96@gmail.com>
2025-03-03 19:30:43 -0300

    Schemas created!

---
+++ src/modules/book/schemas/book.schema.ts
@@ -0,0 +1,35 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { Document } from "mongoose";
+import { User } from "src/modules/user/schemas/user.schema";
+
+
+@Schema()
+export class Book extends Document {
+    @Prop({required: true})
+    title: string;
+
+    @Prop({required: true})
+    author: string;
+
+    @Prop()
+    genre: string;
+
+    @Prop()
+    coverImage: string;
+
+    @Prop({default:'pending'})
+    status: string;
+
+    @Prop({default: 0})
+    pagesRead: number;
+
+    @Prop()
+    totalPages: number;
+
+    @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'User'})
+    user: User;
+    
+
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book)
---
+++ src/modules/review/schemas/review.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose from "mongoose";
+import { Book } from "src/modules/book/schemas/book.schema";
+import { User } from "src/modules/user/schemas/user.schema";
+
+
+@Schema()
+export class Review extends Document {
+
+    @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'Book'})
+    book: Book;
+
+    @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'User'})
+    user: User;
+
+    @Prop({required: true, min: 1,  max: 5})
+    rating: number;
+
+    @Prop()
+    comment: string;
+}
+
+export const ReviewSchema = SchemaFactory.createForClass(Review);
